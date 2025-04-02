dcd15cad8ce5da2147f955cced67b7d2b3154eb5
Ediz Arkın Kobak <119970256+EdizArkin@users.noreply.github.com>
2025-04-02 18:42:12 +0300

    PostgreSQL Fixed

--- YJTBackend/Program.cs
+++ YJTBackend/Program.cs
@@ -1,14 +1,31 @@
-using YJTBackend.Data;
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
+using YJTBackend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Load .env file
+Env.Load(); // Bu satır, .env dosyasındaki verileri yükler
+
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var port = Environment.GetEnvironmentVariable("DB_PORT");
+var db = Environment.GetEnvironmentVariable("DB_NAME");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var pass = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(port) || string.IsNullOrEmpty(db) || string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pass))
+{
+    throw new InvalidOperationException("One or more environment variables are missing.");
+}
+
+var connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={pass}";
+
 // Add database context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
+
 app.UseRouting();
 app.MapControllers();
 app.Run();
- 
